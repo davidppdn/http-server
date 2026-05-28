fc197ca5e818d6ed34352f0f5a66a9521d8c7da7
David Pedan <david.ppdn@gmail.com>
2026-05-28 15:54:13 +1000

    Basic TCP-based HTTP server.
    - Binds a socket to a port and listens for incoming TCP connections
    - Accepts incoming connections after TCP handshake completion
    - Wraps each connection in a TcpClient providing a NetworkStream
    - Reads a continuous byte stream from the client socket
    - Processes the raw data (parsing and transformation)
    - Writes a response back over the same TCP connection

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -1,1 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    private static readonly Int32 _port = 13000;
+    private static readonly IPAddress _address = IPAddress.Parse("127.0.0.1");
+
+    static void Main(string[] args)
+    {
+        TcpListener? server = null;
+        try
+        {
+            // Create a TCP/IP socket, and start listening for incoming connections.
+            server = new(_address, _port);
+            server.Start();
+
+            // Buffer for reading data
+            Byte[] bytes = new Byte[256];
+            String? data = null;
+
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                using TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    data = data.ToUpper();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
